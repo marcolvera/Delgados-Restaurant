66184638892765e548d4d2c505d3f74bb43b40c1
Marco Olvera <marco@Marcos-MacBook.lan1>
2020-04-14 13:52:46 -0700

    create index ctrl and link ctrl with route

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,7 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var homeCtrl = require("../controllers/home");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get("/", homeCtrl.index);
 
 module.exports = router;
